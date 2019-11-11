2436599ea11fec93d74fabe54c2c430a885e479e
Manschki <seifriedsbergerm0123@gmail.com>
2019-11-11 12:19:05 +0100

    Compilercode added

--- src/umja/Compiler.java
+++ src/umja/Compiler.java
@@ -1,5 +1,6 @@
 package umja;
 
+import java.io.File;
 import java.util.List;
 
 public class Compiler {
@@ -11,6 +12,45 @@ public class Compiler {
     }
 
     public void compile(String path, List<UMLClazz> umlClazzes) {
+        for (UMLClazz clazz :
+                umlClazzes) {
+            File file = new File(path);
 
+
+            //write package
+            StringBuilder builder = new StringBuilder();
+            builder.append("package ");
+            builder.append(clazz.getStrPackage());
+            builder.append(";\n");
+
+            //TODO Imports???????
+
+            //write class notation
+            builder.append("public ");
+            builder.append(clazz.getClassType().toString().toLowerCase());
+            builder.append(" ");
+            builder.append(clazz.getClazzName());
+
+            //interfaces and inheritance
+            if (clazz.getInheritsFrom() != null) {
+                builder.append(" extends ")
+                        .append(clazz.getInheritsFrom());
+            }
+            if (!clazz.getInterfaces().isEmpty()) {
+                builder.append(" implements ")
+                        .append(String.join(", ", clazz.getInterfaces()))
+                        .append(" ");
+            }
+
+            builder.append(" {\n");
+
+            //TODO properites
+
+            //TODO Constructor and Methods
+
+
+            //close bracket from class
+            builder.append("}");
+        }
     }
 }
