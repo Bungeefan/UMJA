375ec6609edde928b7adf5195ceaab677436b436
Bungeefan <severin.hamader@yahoo.de>
2019-11-12 12:07:33 +0100

    Added File Output

--- src/umja/Compiler.java
+++ src/umja/Compiler.java
@@ -1,6 +1,9 @@
 package umja;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Modifier;
+import java.nio.file.Files;
 import java.util.List;
 
 public class Compiler {
@@ -13,83 +16,93 @@ public class Compiler {
 
     public void compile(String path, List<UMLClazz> umlClazzes) {
         for (UMLClazz clazz : umlClazzes) {
+            File file = new File(path + File.separator + clazz.getStrPackage().replace(".", File.separator) + File.separator + clazz.getClazzName() + ".java");
+            if (file.mkdirs()) {
 
-            //write package
-            StringBuilder builder = new StringBuilder();
-            builder.append("package ");
-            builder.append(clazz.getStrPackage());
-            builder.append(";\n");
+                //write package
+                StringBuilder builder = new StringBuilder();
+                builder.append("package ");
+                builder.append(clazz.getStrPackage());
+                builder.append(";\n");
 
-            //TODO Imports???????
+                //TODO Imports???????
 
-            //write class notation
-            builder.append("public ");
-            builder.append(clazz.getClassType().toString().toLowerCase());
-            builder.append(" ");
+                //write class notation
+                builder.append("public ");
+                builder.append(clazz.getClassType().toString().toLowerCase());
+                builder.append(" ");
 
-            builder.append(clazz.getClazzName());
+                builder.append(clazz.getClazzName());
 
-            //interfaces and inheritance
-            if (clazz.getInheritsFrom() != null) {
-                builder.append(" extends ")
-                        .append(clazz.getInheritsFrom());
-            }
-            if (!clazz.getInterfaces().isEmpty()) {
-                builder.append(" implements ")
-                        .append(String.join(", ", clazz.getInterfaces()))
-                        .append(" ");
-            }
+                //interfaces and inheritance
+                if (clazz.getInheritsFrom() != null) {
+                    builder.append(" extends ")
+                            .append(clazz.getInheritsFrom());
+                }
+                if (!clazz.getInterfaces().isEmpty()) {
+                    builder.append(" implements ")
+                            .append(String.join(", ", clazz.getInterfaces()))
+                            .append(" ");
+                }
 
-            builder.append(" {\n");
+                builder.append(" {\n");
 
-            if (!clazz.getProperties().isEmpty()) {
-                for (UMLClazzProperty property : clazz.getProperties()) {
-                    if (clazz.getClassType() == UMLClazz.ClassType.ENUM) {
-                        builder.append(property.getName());
-                    } else {
-                        if (property.getModifier() == Modifier.PUBLIC) {
+                if (!clazz.getProperties().isEmpty()) {
+                    for (UMLClazzProperty property : clazz.getProperties()) {
+                        if (clazz.getClassType() == UMLClazz.ClassType.ENUM) {
+                            builder.append(property.getName());
+                        } else {
+                            if (property.getModifier() == Modifier.PUBLIC) {
+                                builder.append("public ");
+                            } else if (property.getModifier() == Modifier.PRIVATE) {
+                                builder.append("private ");
+                            } else if (property.getModifier() == Modifier.PROTECTED) {
+                                builder.append("protected ");
+                            }
+                            builder.append(property.getDataType());
+                            builder.append(" ");
+                            builder.append(property.getName());
+                            builder.append(";\n");
+                        }
+                    }
+                }
+
+                if (!clazz.getMethods().isEmpty()) {
+                    for (UMLClazzMethod method : clazz.getMethods()) {
+                        if (method.getModifier() == Modifier.PUBLIC) {
                             builder.append("public ");
-                        } else if (property.getModifier() == Modifier.PRIVATE) {
+                        } else if (method.getModifier() == Modifier.PRIVATE) {
                             builder.append("private ");
-                        } else if (property.getModifier() == Modifier.PROTECTED) {
+                        } else if (method.getModifier() == Modifier.PROTECTED) {
                             builder.append("protected ");
                         }
-                        builder.append(property.getDataType());
-                        builder.append(" ");
-                        builder.append(property.getName());
-                        builder.append(";\n");
+                        if (method.getReturnValue() != null) {
+                            builder.append(method.getReturnValue());
+                            builder.append(" ");
+                        }
+                        builder.append(method.getName());
+                        builder.append("(");
+                        if (!method.getParameter().isEmpty()) {
+                            builder.append(String.join(", ", method.getParameter()));
+                        }
+                        builder.append(")");
+                        builder.append("{\n");
+                        builder.append("\n}\n");
                     }
                 }
-            }
 
-            if (!clazz.getMethods().isEmpty()) {
-                for (UMLClazzMethod method : clazz.getMethods()) {
-                    if (method.getModifier() == Modifier.PUBLIC) {
-                        builder.append("public ");
-                    } else if (method.getModifier() == Modifier.PRIVATE) {
-                        builder.append("private ");
-                    } else if (method.getModifier() == Modifier.PROTECTED) {
-                        builder.append("protected ");
-                    }
-                    if (method.getReturnValue() != null) {
-                        builder.append(method.getReturnValue());
-                        builder.append(" ");
-                    }
-                    builder.append(method.getName());
-                    builder.append("(");
-                    if (!method.getParameter().isEmpty()) {
-                        builder.append(String.join(", ", method.getParameter()));
-                    }
-                    builder.append(")");
-                    builder.append("{\n");
-                    builder.append("\n}\n");
+                //close bracket from class
+                builder.append("}");
+
+                try {
+                    Files.writeString(file.toPath(), builder.toString());
+                } catch (IOException e) {
+                    fxmlDocumentController.log(e.getClass().getSimpleName() + ": " + e.getMessage());
+                    e.printStackTrace();
                 }
+            } else {
+                fxmlDocumentController.log(clazz.getClazzName() + " failed to create parent dir!");
             }
-
-            //close bracket from class
-            builder.append("}");
-
-            System.out.println(builder.toString());
         }
     }
 }
