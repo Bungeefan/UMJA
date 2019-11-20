3d5528edcdd33d7cf230c1bd4ce7dc88f596c243
Manschki <seifriedsbergerm0123@gmail.com>
2019-11-20 16:45:37 +0100

    Fixed Issues with the format in the Output

--- src/umja/Compiler.java
+++ src/umja/Compiler.java
@@ -16,6 +16,7 @@ public class Compiler {
     }
 
     public void compile(String path, List<UMLClazz> umlClazzes) {
+        String oneTab = "    ";
         for (UMLClazz clazz : umlClazzes) {
             File file = new File(path + File.separator + clazz.getStrPackage().replace(".", File.separator) + File.separator + clazz.getClazzName() + ".java");
             try {
@@ -25,7 +26,7 @@ public class Compiler {
                 StringBuilder builder = new StringBuilder();
                 builder.append("package ");
                 builder.append(clazz.getStrPackage());
-                builder.append(";\n");
+                builder.append(";\n\n");
 
                 //TODO Imports???????
 
@@ -50,9 +51,17 @@ public class Compiler {
                 builder.append(" {\n");
 
                 if (!clazz.getProperties().isEmpty()) {
+                    if (clazz.getClassType() == UMLClazz.ClassType.ENUM) {
+                        builder.append(oneTab);
+                    }
                     for (UMLClazzProperty property : clazz.getProperties()) {
+                        if (clazz.getClassType() != UMLClazz.ClassType.ENUM) {
+                            builder.append(oneTab);
+                        }
                         if (clazz.getClassType() == UMLClazz.ClassType.ENUM) {
+
                             builder.append(property.getName());
+
                         } else {
                             if (property.getModifier() == Modifier.PUBLIC) {
                                 builder.append("public ");
@@ -67,10 +76,12 @@ public class Compiler {
                             builder.append(";\n");
                         }
                     }
+                    builder.append("\n");
                 }
 
                 if (!clazz.getMethods().isEmpty()) {
                     for (UMLClazzMethod method : clazz.getMethods()) {
+                        builder.append(oneTab);
                         if (method.getModifier() == Modifier.PUBLIC) {
                             builder.append("public ");
                         } else if (method.getModifier() == Modifier.PRIVATE) {
@@ -89,7 +100,10 @@ public class Compiler {
                         }
                         builder.append(")");
                         builder.append("{\n");
-                        builder.append("\n}\n");
+                        builder.append("\n");
+                        builder.append(oneTab);
+                        builder.append("}\n");
+                        builder.append("\n");
                     }
                 }
 
